0f950a84c205ab403b13fc8a279369303c617e66
taco710 <kota.yamashita@rakuten-sec.co.jp>
2021-09-09 16:21:54 +0900

    課題

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('xxx', 'AAA\Controller@bbb');
+
+Route::get('admin/profile/create', 'Admin\profileController@create');
+
+Route::get('admin/profile/edit', 'Admin\profileController@edit');
+
+
+
+
+
+
+
