ad80beef67730ed6f3f5b1c52f7088bbd5d33d78
taco710 <kota.yamashita@rakuten-sec.co.jp>
2021-09-09 16:15:28 +0900

    課題

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Myfile</title>
+    </head>
+    <body>
+        <h1>Myニュース画面</h1>
+    </body>
+</html>
